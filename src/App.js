89dbe5d5990e7a59c97491cc8998d889fb6de693
jsn-lps <lpsjsn94@gmail.com>
2022-09-11 21:19:48 -0400

    tymur - a customizable 25-5 clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="body">
+
+        <div id="timer-label"></div>
+
+        <div id="time-left">00:00</div>
+        <div id="start_stop">pause/play</div>
+        <div id="reset">reset</div>
+
+
+      <div id="yuh"></div>
+      <div id="break-label">Break Length</div>
+        <button id="break-decrement">-</button>
+        <div id="break-length">5 default</div>
+        <button id="break-increment">+</button>
+
+
+      <div id="session-label">Session Length</div>
+        <button id="session-decrement">-</button>
+        <div id="session-length">25 default</div>
+        <button id="session-increment">+</button>
+
+
+      </div>
     </div>
   );
 }
